7bb6dba1d33d39e563fd330e7a2e31dd20fdd158
unknown <khanhigoufake2@gmail.com>
2020-01-23 21:04:29 +0100

    edit props

--- frontend/src/components/DeletePost.js
+++ frontend/src/components/DeletePost.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Button } from "@material-ui/core/Button";
+import Button from "@material-ui/core/Button";
 import "./DeletePost.css";
 
 export default class DeletePost extends Component {
