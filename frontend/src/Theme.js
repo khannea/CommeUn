4565dff8a2823a47c6abef4b26e0247bd3b2da0e
unknown <khanhigoufake2@gmail.com>
2020-01-27 15:51:49 +0100

    OriginPost new tpe

--- frontend/src/Theme.js
+++ frontend/src/Theme.js
@@ -43,19 +43,21 @@ export default createMuiTheme({
         }
       }
     },
-    card: {
-      minWidth: 275
-    },
-    bullet: {
-      display: "inline-block",
-      margin: "0 2px",
-      transform: "scale(0.8)"
-    },
-    title: {
-      fontSize: 14
-    },
-    pos: {
-      marginBottom: 12
+    MuiCard: {
+      root: {
+        minWidth: 275
+      },
+      bullet: {
+        display: "inline-block",
+        margin: "0 2px",
+        transform: "scale(0.8)"
+      },
+      title: {
+        fontSize: 14
+      },
+      pos: {
+        marginBottom: 12
+      }
     }
   }
 });
