e9fbc99193ef0b1e8a98826dcf17db5d5405ac6c
unknown <khanhigoufake2@gmail.com>
2020-01-30 00:45:41 +0100

    New post style

--- frontend/src/layouts/Layout.js
+++ frontend/src/layouts/Layout.js
@@ -15,6 +15,7 @@ import AccessError from "../views/AccessError";
 import UsersPage from "../views/UsersPage";
 import ForumView from "../views/ForumView";
 import TopicView from "../views/TopicView";
+import PostView from "../views/PostView";
 import MessageView from "../views/MessageView";
 const switchRoutes = (
   <Switch>
@@ -24,7 +25,8 @@ const switchRoutes = (
     <Route path="/Layout/UsersPage" component={WithAuth(UsersPage)} />
     <Route path="/Layout/ForumView" component={WithAuth(ForumView)} />
     <Route path="/Layout/MessageView" component={WithAuth(MessageView)} />
-    <Route path="/Layout/Topic/:id" component={TopicView} />
+    <Route path="/Layout/Topic/:id" component={PostView} />
+    {/* <Route path="/Layout/Topic/:id" component={TopicView} /> */}
     <Route path="/Layout/Message/:id" component={MessageView} />
     <Redirect to="/Layout/Presentation" />
   </Switch>
---
+++ frontend/src/views/PostView.js
@@ -0,0 +1,113 @@
+import React, { Component } from "react";
+import "./PostView.css";
+import Post from "../components/Post";
+
+class PostView extends Component {
+  state = {
+    type: "best",
+    data: null
+  };
+
+  constructor(props) {
+    super(props);
+    props = this.props;
+    if (!this.origin_id) {
+      this.origin_id = props.location.pathname.split("/")[3];
+      this.setState({ type: "all" });
+    }
+  }
+
+  componentDidMount() {
+    this.getDataFromDb();
+  }
+
+  getDataFromDb = () => {
+    let url = "/api/origin_posts/" + this.origin_id;
+    let req = new Request(url, {
+      method: "GET",
+      cache: "default",
+      credentials: "include"
+    });
+    fetch(req)
+      .then(res => {
+        if (res.status === 401) {
+          console.log("getDataFromDb n'a pas recu les topics.");
+        } else {
+          return res.json();
+        }
+      })
+      .then(data => {
+        this.setState({
+          data: data
+        });
+      });
+  };
+
+  compareValues = (key, order = "asc") => {
+    return function innerSort(a, b) {
+      if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
+        // property doesn't exist on either object
+        return 0;
+      }
+
+      const varA = typeof a[key] === "string" ? a[key].toUpperCase() : a[key];
+      const varB = typeof b[key] === "string" ? b[key].toUpperCase() : b[key];
+
+      let comparison = 0;
+      if (varA > varB) {
+        comparison = 1;
+      } else if (varA < varB) {
+        comparison = -1;
+      }
+      return order === "desc" ? comparison * -1 : comparison;
+    };
+  };
+
+  render() {
+    let { data } = this.state;
+
+    if (data) {
+      data.sort(this.compareValues("likes", "desc"));
+    }
+
+    return (
+      <div className="post_wrapper">
+        {data &&
+          type === "all" &&
+          data.map(({ user, texte, date, id, likes, dislikes }, index) => (
+            <div key={index}>
+              <Post
+                user={user}
+                texte={texte}
+                date={date}
+                key={index}
+                id={id}
+                likes={likes}
+                dislikes={dislikes}
+                refresh={this.getDataFromDb}
+              />
+              <PostView origin_id={id} />
+            </div>
+          ))}
+
+        {data && type === "best" && (
+          <div>
+            <Post
+              user={data[0].user}
+              texte={data[0].texte}
+              date={data[0].date}
+              key={data[0].index}
+              id={data[0].id}
+              likes={data[0].likes}
+              dislikes={data[0].dislikes}
+              refresh={this.getDataFromDb}
+            />
+            <PostView origin_id={data[0].id} />
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default PostView;
