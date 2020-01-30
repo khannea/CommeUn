cf6c4865bc9093fc5754249f6654eeef7728858b
unknown <khanhigoufake2@gmail.com>
2020-01-30 01:10:49 +0100

    New post style

--- frontend/src/views/PostView.js
+++ frontend/src/views/PostView.js
@@ -91,7 +91,7 @@ class PostView extends Component {
                 dislikes={dislikes}
                 refresh={this.getDataFromDb}
               />
-              {/* <PostView origin_id={id} /> */}
+              <PostView origin_id={id} />
             </div>
           ))}
 
@@ -107,7 +107,7 @@ class PostView extends Component {
               dislikes={data[0].dislikes}
               refresh={this.getDataFromDb}
             />
-            {/* <PostView origin_id={data[0].id} /> */}
+            <PostView origin_id={data[0].id} />
           </div>
         )}
       </div>
