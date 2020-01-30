f5e07c56a41a264c032b386cb001aed3f6d3112a
unknown <khanhigoufake2@gmail.com>
2020-01-30 01:38:05 +0100

    New post style

--- frontend/src/views/PostView.js
+++ frontend/src/views/PostView.js
@@ -101,7 +101,13 @@ class PostView extends Component {
                 refresh={this.getDataFromDb}
                 origin_id={id}
               />
-              <Button onClick={this.switchType()}>...</Button>
+              <Button
+                onClick={() => {
+                  this.switchType();
+                }}
+              >
+                ...
+              </Button>
             </div>
           ))}
 
@@ -118,7 +124,13 @@ class PostView extends Component {
               refresh={this.getDataFromDb}
               origin_id={data[0].id}
             />
-            <Button onClick={this.switchType()}>...</Button>
+            <Button
+              onClick={() => {
+                this.switchType();
+              }}
+            >
+              ...
+            </Button>
           </div>
         )}
       </div>
