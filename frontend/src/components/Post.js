5bddb3544e8abfb53a2713435087557b67f2a390
unknown <khanhigoufake2@gmail.com>
2020-02-03 16:49:23 +0100

    test comment

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -63,36 +63,32 @@ class Post extends Component {
   renderLikeDislike = () => {
     return (
       <div>
-        <div>
-          <Col>
-            <Image
-              className="banniere"
-              src={require("./like.png")}
-              width="20px"
-              height="20px"
-              style={{ cursor: "pointer" }}
-              onClick={this.onClickLike}
-            />
-          </Col>
-          <Col>
-            <b>&nbsp;{this.props.likes}</b>
-          </Col>
-        </div>
-        <div>
-          <Col>
-            <Image
-              className="banniere"
-              src={require("./dislike.png")}
-              width="20px"
-              height="20px"
-              style={{ cursor: "pointer" }}
-              onClick={this.onClickDislike}
-            />
-          </Col>
-          <Col>
-            <b>&nbsp;{this.props.dislikes}</b>
-          </Col>
-        </div>
+        <Col>
+          <Image
+            className="banniere"
+            src={require("./like.png")}
+            width="20px"
+            height="20px"
+            style={{ cursor: "pointer" }}
+            onClick={this.onClickLike}
+          />
+        </Col>
+        <Col>
+          <b>&nbsp;{this.props.likes}</b>
+        </Col>
+        <Col>
+          <Image
+            className="banniere"
+            src={require("./dislike.png")}
+            width="20px"
+            height="20px"
+            style={{ cursor: "pointer" }}
+            onClick={this.onClickDislike}
+          />
+        </Col>
+        <Col>
+          <b>&nbsp;{this.props.dislikes}</b>
+        </Col>
       </div>
     );
   };
