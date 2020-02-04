30a3dbfe38df9375f8ce64608fa55869b2c0478c
unknown <khanhigoufake2@gmail.com>
2020-02-04 18:24:56 +0100

    topicxView

--- frontend/src/components/MainTopic.js
+++ frontend/src/components/MainTopic.js
@@ -80,32 +80,34 @@ class MainTopic extends Component {
   renderLikeDislike = () => {
     return (
       <div>
-        <Col>
-          <Image
-            className="banniere"
-            src={require("./like.png")}
-            width="20px"
-            height="20px"
-            style={{ cursor: "pointer" }}
-            onClick={this.onClickLike}
-          />
-        </Col>
-        <Col>
-          <b>&nbsp;{this.props.likes}</b>
-        </Col>
-        <Col>
-          <Image
-            className="banniere"
-            src={require("./dislike.png")}
-            width="20px"
-            height="20px"
-            style={{ cursor: "pointer" }}
-            onClick={this.onClickDislike}
-          />
-        </Col>
-        <Col>
-          <b>&nbsp;{this.props.dislikes}</b>
-        </Col>
+        <Row>
+          <Col>
+            <Image
+              className="banniere"
+              src={require("./like.png")}
+              width="20px"
+              height="20px"
+              style={{ cursor: "pointer" }}
+              onClick={this.onClickLike}
+            />
+          </Col>
+          <Col>
+            <b>&nbsp;{this.props.likes}</b>
+          </Col>
+          <Col>
+            <Image
+              className="banniere"
+              src={require("./dislike.png")}
+              width="20px"
+              height="20px"
+              style={{ cursor: "pointer" }}
+              onClick={this.onClickDislike}
+            />
+          </Col>
+          <Col>
+            <b>&nbsp;{this.props.dislikes}</b>
+          </Col>
+        </Row>
       </div>
     );
   };
