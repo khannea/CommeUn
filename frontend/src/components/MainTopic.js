cee2ed1dde896f16a6397ccab67f2c94380678f7
unknown <khanhigoufake2@gmail.com>
2020-02-04 18:18:12 +0100

    topicxView

---
+++ frontend/src/components/MainTopic.js
@@ -0,0 +1,144 @@
+import React, { Component } from "react";
+import { Row, Col, Image } from "react-bootstrap";
+import "./MainTopic.css";
+import { Card, CardHeader, CardContent } from "@material-ui/core";
+
+class MainTopic extends Component {
+  state = {
+    id: null,
+    data: null
+  };
+
+  componentDidMount() {
+    this.setState(
+      { id: this.props.location.pathname.split("/")[3] },
+      this.getDataFromDb()
+    );
+  }
+
+  getDataFromDb = () => {
+    let url = "/api/topic/" + this.props.id;
+    let req = new Request(url, {
+      method: "GET",
+      cache: "default",
+      credentials: "include"
+    });
+    fetch(req)
+      .then(res => {
+        if (res.status === 401) {
+          console.log("getDataFromDb n'a pas recu les topics.");
+        } else {
+          return res.json();
+        }
+      })
+      .then(data => {
+        this.setState({
+          data: data
+        });
+      });
+  };
+
+  onClickLike = () => {
+    fetch("/api/likepost", {
+      method: "POST",
+      body: JSON.stringify({ msgId: this.props.id, user: this.user }),
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(response => {
+        if (response.status === 401) {
+          console.log("Probleme de like");
+        } else {
+          this.props.refresh();
+        }
+      })
+      .catch(error => console.error("Error:", error));
+  };
+
+  onClickDislike = () => {
+    fetch("/api/dislikepost", {
+      method: "POST",
+      body: JSON.stringify({ msgId: this.props.id, user: this.user }),
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(response => {
+        if (response.status === 401) {
+          console.log("Probleme de dislike");
+        } else {
+          this.props.refresh();
+        }
+      })
+      .catch(error => console.error("Error:", error));
+  };
+
+  renderLikeDislike = () => {
+    return (
+      <div>
+        <Col>
+          <Image
+            className="banniere"
+            src={require("./like.png")}
+            width="20px"
+            height="20px"
+            style={{ cursor: "pointer" }}
+            onClick={this.onClickLike}
+          />
+        </Col>
+        <Col>
+          <b>&nbsp;{this.props.likes}</b>
+        </Col>
+        <Col>
+          <Image
+            className="banniere"
+            src={require("./dislike.png")}
+            width="20px"
+            height="20px"
+            style={{ cursor: "pointer" }}
+            onClick={this.onClickDislike}
+          />
+        </Col>
+        <Col>
+          <b>&nbsp;{this.props.dislikes}</b>
+        </Col>
+      </div>
+    );
+  };
+
+  render() {
+    let { data, id } = this.state;
+    // .split("\n")
+    // .map((item, i) => <p key={i}>{item}</p>);
+
+    return (
+      <Card className="mx-4 my-2" elevation="0">
+        {data && data.length > 0 && (
+          <CardContent>
+            <Row>
+              {/* <Col id="avatar_wrapper" className="col-auto">
+              <Avatar src="/broken-image.jpg" />
+            </Col> */}
+              <Col className="border-right col1st  col-auto">
+                <Row className="h-100">
+                  <Col id="like_dislike_wrapper">
+                    {/* {this.renderLikeDislike(data)} */}
+                  </Col>
+                </Row>
+              </Col>
+              <Col id="post_content_wrapper" className="border-dark">
+                <CardHeader title={data[0].user + " " + data[0].date} />
+                {data[0].texte}
+              </Col>
+            </Row>
+          </CardContent>
+        )}
+      </Card>
+    );
+  }
+}
+
+export default MainTopic;
--- frontend/src/views/TopicView.js
+++ frontend/src/views/TopicView.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import "./TopicView.css";
 import PostView from "./PostView";
+import MainTopic from "../components/MainTopic";
 
 class TopicView extends Component {
   render() {
     return (
       <div id="topic_wrapper">
+        <MainTopic />
         <PostView />
       </div>
     );
