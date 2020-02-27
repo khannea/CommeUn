9b251ffbda0b1936df17c0ef53fc2dc55a129970
unknown <khanhigoufake2@gmail.com>
2020-02-27 01:46:15 +0100

    change sidebar 1st

--- frontend/src/views/Curriculum.jsx
+++ frontend/src/views/Curriculum.jsx
@@ -1,6 +1,17 @@
 import React, { Component } from "react";
+import anime from "animejs";
 
 class Curriculum extends Component {
+  constructor() {
+    super();
+  }
+  componentDidMount() {
+    anime({
+      targets: ".css-selector-demo .el",
+      translateX: window.innerWidth
+    });
+  }
+
   render() {
     return <div id="wrapper_curriculum">Pensée numéro 1</div>;
   }
